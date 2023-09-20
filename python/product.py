ddaa7dd93af6c244cf00c3647a798be914ee269a
Gabe-rep <139786071+Gabe-rep@users.noreply.github.com>
2023-09-20 13:01:14 +0200

    Add files via upload

--- python/product.py
+++ python/product.py
@@ -1,7 +1,7 @@
 from ImageAnalysis import*
 import re
 
-def prod ():
+def prod():
     tags = image_analysis() 
     #print (tags)
 
@@ -13,15 +13,24 @@ def prod ():
     filepath = r"C:\Users\GabinStewellSimoKams\Desktop\AI-moda\HttpTrigger1\file.txt"
     fileObject = open(filepath, 'r',encoding = 'utf-8')
     txt_html = fileObject.read()
-    print (txt_html)
-
-    # estrazione del vestito con filtro 
+    #print (txt_html)
+    # estrazione del vestito con filtro
     try :
-        prod = [req for req in re.findall(r"(?<=)wearing a(.*?)and", txt_html)][0]
-        print(prod)
-        return prod
+        prod1 = [req for req in re.findall(r"(?<=)wearing a(.*)", txt_html)]
+        #print (prod1)
+        prod2 = [req for req in re.findall(r"(?<=)up of a(.*)", txt_html)]
+        #print (prod2)
+        prod3 = [req for req in re.findall(r"(?<=)pair of(.*)", txt_html)]
+        #print (prod3)
+        prod4 = [req for req in re.findall(r"(?<=)in a(.*)", txt_html)]
+        #print (prod4)
+        prod5 = [req for req in re.findall(r"(?<=)a pair of(.*?)with", txt_html)]
+        #print (prod5)
+        lista_prod = [prod1, prod2, prod3, prod4, prod5]
+        return lista_prod
     except :
         IndexError
-
+    
+    
 
 
