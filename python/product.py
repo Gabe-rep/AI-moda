3ec48a195b397fa7b10481d84731f063fed9722d
Gabe-rep <139786071+Gabe-rep@users.noreply.github.com>
2023-09-20 12:58:07 +0200

    Add files via upload

--- python/product.py
+++ python/product.py
@@ -4,20 +4,22 @@ import re
 def prod ():
     tags = image_analysis() 
     #print (tags)
-    
+
     #salvare tutti i tags in un file txt
-    with open('file.txt', 'w') as f:
+    with open(r"C:\Users\GabinStewellSimoKams\Desktop\AI-moda\HttpTrigger1\file.txt", "w") as f:
         for tags in tags:
             f.write("%s\n" % tags)
-
     #estrare la parola da cercare
-    filepath = r"C:\Users\GabinStewellSimoKams\Desktop\AI-moda\file.txt"
+    filepath = r"C:\Users\GabinStewellSimoKams\Desktop\AI-moda\HttpTrigger1\file.txt"
     fileObject = open(filepath, 'r',encoding = 'utf-8')
     txt_html = fileObject.read()
     print (txt_html)
+
+    # estrazione del vestito con filtro 
     try :
-        prod = [req for req in re.findall(r"(?<=)wearing a(.*)", txt_html)][0]
-        print (prod)
+        prod = [req for req in re.findall(r"(?<=)wearing a(.*?)and", txt_html)][0]
+        print(prod)
+        return prod
     except :
         IndexError
 
--- python/response.py
+++ python/response.py
@@ -6,32 +6,65 @@ import time
 import urllib.request as ur
 
 
-def get_zalando_response () :
+def get_zalando_response() :
+    
+    test = prod()
+    if len(test[0]) != 0 :
+        for i in test[0]:
+            name_prod = i
 
-    name_prod = prod()
+            url = ur.urlopen(f"https://www.zalando.it/catalogo/?q={name_prod.strip().replace(' ', '+')}").read()
+            string = bytes.decode(url, 'utf-8')
+            print (f"https://www.zalando.it/catalogo/?q={name_prod.strip().replace(' ', '+')}")
 
-    url = ur.urlopen(f"https://www.zalando.it/catalogo/?q={name_prod.strip().replace(' ', '+')}").read()
-    string = bytes.decode(url, 'utf-8')
+    if len(test[1]) != 0 :
+        for i in test[1]:
+            name_prod = i
+
+            url = ur.urlopen(f"https://www.zalando.it/catalogo/?q={name_prod.strip().replace(' ', '+')}").read()
+            string = bytes.decode(url, 'utf-8')
+            print (f"https://www.zalando.it/catalogo/?q={name_prod.strip().replace(' ', '+')}")
+
+    if len(test[2]) != 0 :
+        for i in test[2]:
+            name_prod = i
+
+            url = ur.urlopen(f"https://www.zalando.it/catalogo/?q={name_prod.strip().replace(' ', '+')}").read()
+            string = bytes.decode(url, 'utf-8')
+            print (f"https://www.zalando.it/catalogo/?q={name_prod.strip().replace(' ', '+')}")
+
+    if len(test[3]) != 0 :
+        for i in test[3]:
+            name_prod = i
+
+            url = ur.urlopen(f"https://www.zalando.it/catalogo/?q={name_prod.strip().replace(' ', '+')}").read()
+            string = bytes.decode(url, 'utf-8')
+            print (f"https://www.zalando.it/catalogo/?q={name_prod.strip().replace(' ', '+')}")
+
+    if len(test[4]) != 0 :
+        for i in test[4]:
+            name_prod = i
+
+            url = ur.urlopen(f"https://www.zalando.it/catalogo/?q={name_prod.strip().replace(' ', '+')}").read()
+            string = bytes.decode(url, 'utf-8')
+            print (f"https://www.zalando.it/catalogo/?q={name_prod.strip().replace(' ', '+')}")
 
 
-    # x = requests.get(url)
-    # content = x.content
-    soup = bs4.BeautifulSoup(string, 'html')
 
 
-    name =  [name for name in re.findall(r"\>(.*?)\<", str(soup.find_all("h3", {"class":"KxHAYs lystZ1 FxZV-M _4F506m ZkIJC- r9BRio qXofat EKabf7 nBq1-s _2MyPg2"})))]
-    azienda_name = [name for name in re.findall(r"\>(.*?)\<", str(soup.find_all("h3",{"class":"_6zR8Lt lystZ1 FxZV-M _4F506m ZkIJC- r9BRio qXofat EKabf7 nBq1-s _2MyPg2"})))]
-    price = [name for name in re.findall(r"\>(.*?)\<", str(soup.find_all("p", {"class":"KxHAYs lystZ1 FxZV-M _4F506m"})))]
-    product = [i for i in name if i not in (', ')]
-    compagny = [e for e in azienda_name if e not in (', ')]
-    price = [x for x in price if x not in (', ')]
+    # # x = requests.get(url)
+    # # content = x.content
+    # soup = bs4.BeautifulSoup(string, 'html')
+    # # print (soup)
+
+    # name =  [name for name in re.findall(r"\>(.*?)\<", str(soup.find_all("h3", {"class":"KxHAYs lystZ1 FxZV-M _4F506m ZkIJC- r9BRio qXofat EKabf7 nBq1-s _2MyPg2"})))]
+    # azienda_name = [name for name in re.findall(r"\>(.*?)\<", str(soup.find_all("h3",{"class":"_6zR8Lt lystZ1 FxZV-M _4F506m ZkIJC- r9BRio qXofat EKabf7 nBq1-s _2MyPg2"})))]
+    # price = [name for name in re.findall(r"\>(.*?)\<", str(soup.find_all("p", {"class":"KxHAYs lystZ1 FxZV-M _4F506m"})))]
+    # product = [i for i in name if i not in (', ')]
+    # compagny = [e for e in azienda_name if e not in (', ')]
+    # prize = [x for x in price if x not in (', ')]
 
 
-    print (product)
-    print (compagny)
-    print (price)
 
-
-get_zalando_response ()
 
 
